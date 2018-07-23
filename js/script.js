23dd55e67adcb07baec7598e1eecb3af1ad691ab
Alicia Quílez <aliciaquilezsayas@gmail.com>
2018-07-23 09:27:56 +0200

    Limpio de comentarios

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
-/*FUNCION PARA CERRAR EL MENU CUANDO SE CLICKE ELEMENTO */
 var links = document.getElementsByClassName('nav-link');
 var element = document.getElementById('navbarTogglerDemo02');
 for (i = 0; i < links.length; i++) {
@@ -17,26 +16,3 @@ for (i = 0; i < links.length; i++) {
     element.classList.remove('show');
   })
 };
-
-
-
-/*var logo = document.getElementById("logo");
-console.log(logo);
-var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-console.log(h);
-
-window.onscroll = function () {
-  console.log('hola');
-  console.log(document.body.scrollTop);
-
-    if  (document.documentElement.scrollTop || document.body.scrollTop >= 2000 ) {
-
-        logo.classList.add("navbar-brand-blue");
-        logo.classList.remove("navbar-brand-black");
-    }
-    else if( document.documentElement.scrollTop || document.body.scrollTop >= 600 ) {
-        logo.classList.add("navbar-brand-orange");
-        logo.classList.remove("navbar-brand-blue");
-    } else{logo.classList.add("navbar-brand-black");console.log('negro');}
-};
-*/
